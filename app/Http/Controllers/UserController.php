b5c6d5cc2586f4011a4f94a18c43467984840574
Adrian Lupescu <lupescua@gmail.com>
2018-06-29 16:52:39 +0200

    Controller ->store method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Experience;
 use App\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
@@ -17,16 +16,20 @@ class UserController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request)
+    public function index()
     {
-
+        // Sends to landing page
+            return view('welcome');       
     }
 
+    public function create(){        
+    }
     public function step2(Request $request)
     {
-
-        return view('user.user_create');
+        //After user uploads a pic, it redirects him back
+        // return view('user.user_create');
     }
+    
     /**
      * Store a newly created resource in storage.
      *
@@ -35,24 +38,33 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
+        // Uploads the user info
+        // if($request->password === $request->password_repeat ){
 
-        if($request->agree_to_terms != true){
+            
             $user =  new User();
-            $user->name = $request->name;
-            $user->password = $request->pass;
+
+              //validation
+            $this->validate($request, [
+                'firstname'=>'required',
+                'lastname'=>'required',
+                'email'=>'required|email',
+                'password'=>'required',
+                'phone_number'=>'required',
+                'image' => 'required|image|mimes:jpeg,png|max:2048|dimensions:min_width=250 px,min_height=250 px',
+            ]);
+
+
+            $user->firstname = $request->firstname;
+            $user->lastname = $request->lastname;
+            $user->password = $request->password;
             $user->email = $request->email;
-            $user->adress_country = $request->adress_country;
-            $user->adress_city = $request->adress_city;
-            $user->adress_state = $request->adress_state;
-            $user->adress_street = $request->adress_street;
-            $user->adress_zip = $request->adress_zip;
-            $user->prefered_language = $request->prefered_language;
-            $user->user_tags = $request->user_tags;
-            if(Input::hasFile('photo')){
-                $file = Input::file('photo');
-                $user->photo = $file->getClientOriginalName();
+            $user->phone_number = $request->phone_nr;
+            if(Input::hasFile('image')){
+                $file = Input::file('image');
+                $user->image = $file->getClientOriginalName();
                 $file->move('img', $file->getClientOriginalName());
-            }
+            };
             $user->save();
 
             event(new Registered($user));
@@ -60,11 +72,18 @@ class UserController extends Controller
 
             //redirect to show page
             return redirect(action('UserController@show',[$user->id]));
-        }
-            return redirect(action('UserController@index'));
+        // }
+
+            //  return redirect(action('UserController@index'));
 
     }
 
+    public function show($id)
+    {
+        $user = User::find($id);
+        return view('welcome',compact('user'));
+
+    }
 
 }
 
